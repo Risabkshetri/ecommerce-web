7f6deb0728631dc388c2724e474e8999c56bf9bc
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-08-28 21:44:06 +0530

    built environment for ecommerce web

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Navbar from "@/components/Navbar";
+export default function Home() {
+  return (
+  <div className="h-1vh w-1vw bg-slate-950">
+  <Navbar />
+  </div>
+  );
+}
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+function Navbar() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Documentation
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+export default Navbar;
