ec15ec49bac5c1b8d8baa6d59e1c0aeed3607756
risabkshetri <risawgc@gmail.com>
2025-02-14 23:07:09 +0530

    font error resolved

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils"
 import AIChatbot from "@/components/Chatbot";
 import Navbar from "@/components/Navbar";
 
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+
 export const metadata: Metadata = {
-  title: "Elctro Store", 
+  title: "Elctro Store",
   description: "AI Based Electro Store",
 };
 
@@ -18,7 +24,8 @@ export default function RootLayout({
     <html lang="en">
       <body 
        className={cn(
-        "min-h-screen bg-background font-sans antialiased"
+        "min-h-screen bg-background font-sans antialiased",
+        inter.variable
       )}
       >
         <Navbar />
