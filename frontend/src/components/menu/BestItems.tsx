a0b235b3b1d70cfcfd33c19adcf7bab07c41177c
risabkshetri <risawgc@gmail.com>
2025-03-13 13:09:43 +0530

    type error in menu

--- frontend/src/components/menu/BestItems.tsx
+++ frontend/src/components/menu/BestItems.tsx
@@ -1,31 +1,31 @@
-import GadgetItem from './MenuItems';
-import electronicsGadgets from "../../../data/dummydata.json";
+// import GadgetItem from './MenuItems';
+// import electronicsGadgets from "../../../data/dummydata.json";
 
-interface Gadget {
-  id: string;
-  name: string;
-  price: number;
-  image: string;
-  category: string;
-}
+// interface Gadget {
+//   id: string;
+//   name: string;
+//   price: number;
+//   image: string;
+//   category: string;
+// }
 
-console.log(electronicsGadgets)
+// console.log(electronicsGadgets)
 
-const BestItems = () => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      {electronicsGadgets.map((item: Gadget) => (
-        <GadgetItem 
-          key={item.id} 
-          id={item.id}
-          name={item.name}
-          price={item.price}
-          image={item.image}
-          category={item.category}
-        />
-      ))}
-    </div>
-  );
-};
+// const BestItems = () => {
+//   return (
+//     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+//       {electronicsGadgets.map((item: Gadget) => (
+//         <GadgetItem 
+//           key={item.id} 
+//           id={item.id}
+//           name={item.name}
+//           price={item.price}
+//           image={item.image}
+//           category={item.category}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
 
-export default BestItems;
+// export default BestItems;
